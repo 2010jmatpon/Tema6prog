98e4a35a1e0cf088a6192034d90d6383ec8ae5f5
2010jmatpon <115532008+2010jmatpon@users.noreply.github.com>
2023-05-12 14:49:36 +0200

    Add files via upload

---
+++ Practica6/Ej3/Ej3.java
@@ -0,0 +1,9 @@
+package Practica6.Ej3;
+
+/*
+El programa se repetirá siete veces porque tenemos tres discos y tres posiciones, sin embargo si el programa tuviese
+más discos es obvio que necesitaría más movimientos, mientras que si tuviese menos discos necesitaría menos movimientos.
+
+De esta  manera nuestro programa quedaría de la siguiente forma
+Hanoi(n) = 2^n + 2^n-1 + 2^n-1 + ......
+ */
